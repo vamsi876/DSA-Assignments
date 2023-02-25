959913229e72a19c010a992f50d6f148f378c58c
vamsi876 <76154284+vamsi876@users.noreply.github.com>
2023-02-25 11:58:03 +0530

    Create BinaryTree.js

---
+++ DsaA2/BinaryTree.js
@@ -0,0 +1,5 @@
+// Create a program that accepts the student data as input from the user. The student data should contain the following information:
+// Student id
+// Student name
+// Student marks in 5 subjects
+// Store this information in a tree data structure where each node represents a single student. The tree should be a binary search tree and use the percentage of each student as a criterion for storing objects. After storing all the information, the user will enter the name of a student and his/her grades should be displayed on the screen.
