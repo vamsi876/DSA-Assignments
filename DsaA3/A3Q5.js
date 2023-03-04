00e8c4c51d308a04f6a8245f928fe9a197eac5fb
vamsi876 <76154284+vamsi876@users.noreply.github.com>
2023-03-04 22:34:19 +0530

    Update A3Q5.js

--- DsaA3/A3Q5.js
+++ DsaA3/A3Q5.js
@@ -2,6 +2,7 @@ function deleteBinaryTree(root) {
     if (!root) {
       return;
     }
+    let rootlength = 0
     let queue = [];
     queue.push(root);
     while (queue.length) {
@@ -14,5 +15,26 @@ function deleteBinaryTree(root) {
       }
       currentNode = null;
     }
+    
   }
+
+
+const root = {
+   value: 1,
+  left: {
+    value: 2,
+    left: { value: 4 },
+    right: { value: 5 }
+  },
+  right: {
+    value: 3,
+    left: { value: 6 },
+    right: { value: 7 }
+  }
+};
+console.log(root)
+
+
+const test = deleteBinaryTree(root);
+console.log(test)
   
